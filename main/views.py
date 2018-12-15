ba77fba66d329295910afb36f36046bf14e0e4f4
Sriram Sami <frizensami@gmail.com>
2018-12-15 18:12:25 +0530

    Add category info from DB to frontend index.html

--- main/views.py
+++ main/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from main.models import Category
 
 # Create your views here.
 def index(request):
     template = loader.get_template('main/index.html')
-    context = {}
+    categories = Category.objects.all()
+    context = { "categories": categories }
     return HttpResponse(template.render(context, request))
